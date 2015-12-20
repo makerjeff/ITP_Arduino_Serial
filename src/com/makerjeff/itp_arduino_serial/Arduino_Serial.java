f2abe2917312d9427f2bb54ffa7818630a80bbbd
Jeff Wu <makerjeffwu@gmail.com>
2015-12-19 23:45:57 -0800

    properly reading in the serial byte using arduino serial.write

--- src/com/makerjeff/itp_arduino_serial/Arduino_Serial.java
+++ src/com/makerjeff/itp_arduino_serial/Arduino_Serial.java
@@ -4,14 +4,17 @@ import processing.serial.*;
 
 public class Arduino_Serial extends PApplet{
 	
-	float diameter = 100.0f;
+	float diameter = 100.0f;	//circle diameter
 	int[] colors = {
-			color(50,50,50), 
-			color(255,0,0), 
-			color(0,255,0), 
-			color(0,0,255)
+			color(240,240,240),	//off white
+			color(50,50,50), 	//gray
+			color(255,0,0), 	//red
+			color(0,255,0), 	//green
+			color(0,0,255)		//blue
 			};
-	int xPos = 0;
+	int xPos = 0;	//position 
+	String myMessage = "This is my text message! I'm just putting in more text"
+			+ " to fill in this block of text to experience wrapping.";
 
 	public static void main(String[] args) {
 		PApplet.main("com.makerjeff.itp_arduino_serial.Arduino_Serial");
@@ -22,20 +25,39 @@ public class Arduino_Serial extends PApplet{
 	} 
 	
 	public void setup(){
+		background(colors[0]);
 		smooth();
+		noStroke();
 		frameRate(30.0f);
 		
-		for(int i = 0; i < colors.length; i++) {
-			print(colors[i] + "\r\n");
-		}
+		//print array list
+		printArray(Serial.list());
+		
+		//INITIALIZE SERIAL
+		String portName = Serial.list()[5];
+		Serial myPort = new Serial(this, portName, 9600);
+
+		
 	} 
 	
 	public void draw(){
-		AnimateCircle();	
+		//AnimateCircle();
+		textSize(20);
+		fill(0,0,0);
+		text(myMessage, 30, 45, 300,300);
+
 	}
 	
-	//CUSTOM FUNCTIONS
-	//animata the circle
+	//special Serial Event that's fired when there's serial data
+	public void serialEvent (Serial myPort){
+		//grab the byte
+		int inByte = myPort.read();
+		//print it out
+		print(inByte + "\r\n");
+	}
+	
+	//== CUSTOM FUNCTIONS ==
+	//animate the circle
 	public void AnimateCircle(){
 		clear();
 		background(colors[0]);
